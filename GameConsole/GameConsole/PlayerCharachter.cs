e7bc5f6b203000de4799b9642af82b5a643d82fb
Rehan Memon <rehan4747@gmail.com>
2021-02-03 13:50:10 +0530

    Worked with Nullable Value

--- GameConsole/GameConsole/PlayerCharachter.cs
+++ GameConsole/GameConsole/PlayerCharachter.cs
@@ -7,13 +7,13 @@ namespace GameConsole
     class PlayerCharachter
     {
         public string Name { get; set; } 
-        public int DaysSinceLastLogin { get; set; } 
-        public DateTime DateOfBirth { get; set; }
+        public Nullable<int> DaysSinceLastLogin { get; set; } 
+        public Nullable<DateTime> DateOfBirth { get; set; }
 
         public PlayerCharachter()
         {
-            DaysSinceLastLogin = -1;            //Magic Number
-            DateOfBirth = DateTime.MinValue;    //Magic Number
+            DaysSinceLastLogin = null; 
+            DateOfBirth = null;
         }
 
 
